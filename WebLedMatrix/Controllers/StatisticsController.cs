b67caecb4c9e562a983e27d8d344c0a0e14ffb26
Mateusz Popielarz <rogoznocraft@gmail.com>
2017-04-06 10:36:06 +0200

    Minor changes

--- WebLedMatrix/Controllers/StatisticsController.cs
+++ WebLedMatrix/Controllers/StatisticsController.cs
@@ -34,9 +34,35 @@ namespace WebLedMatrix.Controllers
             }
             else
             {
-                toShow = this.Session.Skip((page -1) * entriesPerPage).Take(entriesPerPage);
+                toShow = this.Session.Skip((page - 1) * entriesPerPage).Take(entriesPerPage);
             }
             return toShow;
         }
+
+        [Route("Stats/User/{user}/{page}/{entiresPerPage}")]
+        public IEnumerable<StatisticsViewModel> UserSummary(string user, int page = 0, int entriesPerPage = 50)
+        {
+            IEnumerable<StatisticsViewModel> toShow;
+            var values = this.Session.Where(x => x.Name == user);
+            if (page < 0)
+            {
+                throw new ArgumentException(nameof(page));
+            }
+            if (entriesPerPage < 1)
+            {
+                throw new ArgumentException(nameof(entriesPerPage));
+            }
+            if (page == 0)
+            {
+                toShow = values.Take(entriesPerPage);
+            }
+            else
+            {
+                toShow = values.Skip((page - 1) * entriesPerPage).Take(entriesPerPage);
+            }
+            return toShow;
+        }
+
+
     }
 }   
--- WebLedMatrix/Controllers/StatisticsViewModel.cs
+++ WebLedMatrix/Controllers/StatisticsViewModel.cs
@@ -4,6 +4,7 @@ namespace WebLedMatrix.Controllers
 {
     public class StatisticsViewModel
     {
+        public string Name { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public DateTime Commands { get; set; }
